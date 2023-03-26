954e65e343a9d70bfc198c34683e2304bb3619ef
aat <aat8001@gmail.com>
2023-03-27 00:51:10 +0530

    ad styles using attributes args

--- Inception/app.js
+++ Inception/app.js
@@ -1,7 +1,13 @@
 const heading = React.createElement(
-    "h1",
-    {name:'custom_heading',id:'headingId'},
-    "Hey I am paragraph rendered by react"
-  );
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(heading);
+  "h1",
+  {
+    name: "custom_heading",
+    id: "headingId",
+    style: {
+      color: "red",
+    },
+  },
+  "Hey I am paragraph rendered by react"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
