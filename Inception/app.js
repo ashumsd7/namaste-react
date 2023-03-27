44072fa9feef3c06ecb68a857043a1a74f994eab
aat <aat8001@gmail.com>
2023-03-27 11:09:33 +0530

    created nested element using react

--- Inception/app.js
+++ Inception/app.js
@@ -1,14 +1,31 @@
-const heading = React.createElement(
-  "h1",
-  {
-    name: "custom_heading",
-    id: "headingId",
-    style: {
-      color: "red",
-    },
-  },
-  "Hey I am paragraph rendered by react"
+//create nested elements
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+
+  React.createElement(
+    "div",
+    { id: "child" },
+ [   React.createElement("h1", {}, "I am h1 : Sibling"),   React.createElement("h1", {}, "I am h1 : Sibling")]
+  )
 );
-console.log(heading)
+
+console.log("Parent is", parent)
+
+
+
+// const heading = React.createElement(
+//   "h1",
+//   {
+//     name: "custom_heading",
+//     id: "headingId",
+//     style: {
+//       color: "red",
+//     },
+//   },
+//   "Hey I am paragraph rendered by react"
+// );
+// console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
