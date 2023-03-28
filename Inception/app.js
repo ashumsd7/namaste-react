e9088fd73fbcac67967632bd408a8484a12cb767
aat <aat8001@gmail.com>
2023-03-28 22:34:05 +0530

    delete commneted code

--- Inception/app.js
+++ Inception/app.js
@@ -13,19 +13,5 @@ const parent = React.createElement(
 
 console.log("Parent is", parent)
 
-
-
-// const heading = React.createElement(
-//   "h1",
-//   {
-//     name: "custom_heading",
-//     id: "headingId",
-//     style: {
-//       color: "red",
-//     },
-//   },
-//   "Hey I am paragraph rendered by react"
-// );
-// console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
