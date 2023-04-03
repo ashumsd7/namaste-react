78458ed283e3377bda917a327b6931e0d0b5e842
aat <aat8001@gmail.com>
2023-04-04 00:28:35 +0530

    wip:jsx

--- LayingFoundation/app.js
+++ LayingFoundation/app.js
@@ -1,3 +1,14 @@
 //create nested elements
-import React from 'react'
-import  ReactDOM  from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+const heading = React.createElement(
+  "h1",
+  {
+    name: "main-heading",
+  },
+  "I am laying the foundation"
+);
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading)
