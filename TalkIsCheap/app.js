5d413e8f6bfe8670994f554d66aeb8d895fe629f
aat <aat8001@gmail.com>
2023-04-04 21:36:31 +0530

    write inline styles in jsx

--- TalkIsCheap/app.js
+++ TalkIsCheap/app.js
@@ -6,10 +6,7 @@ const Header = () => {
   return (
     <div className="header">
       <div className="logo-container">
-        <img
-          className="logo"
-          src="https://i.ibb.co/4VfV1tf/logo.png"
-        ></img>
+        <img className="logo" src="https://i.ibb.co/4VfV1tf/logo.png"></img>
       </div>
       <div className="nav-items">
         <ul>
@@ -24,10 +21,36 @@ const Header = () => {
   );
 };
 
+const styleCard = {
+  backgroundColor: "#f0f0f0",
+};
+
+const ResCard = () => {
+  return (
+    <div className="res-card" style={styleCard}>
+      <h3>JalJala foods</h3>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        {/* res-card */}
+
+        <ResCard />
+      </div>
+    </div>
+  );
+};
+
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
+      <Body />
     </div>
   );
 };
