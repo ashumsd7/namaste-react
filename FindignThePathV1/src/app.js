8630211f1663764a3206bb55deb01fa504bf2eb9
aat <aat8001@gmail.com>
2023-04-10 00:28:16 +0530

    add router and show err page

--- FindignThePathV1/src/app.js
+++ FindignThePathV1/src/app.js
@@ -3,8 +3,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-
-
+import About from "./components/About";
+import ErrorPage from "./components/ErrorPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const AppLayout = () => {
   return (
@@ -15,5 +16,22 @@ const AppLayout = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  // {
+  //   path: "*",
+  //   element: <ErrorPage />,
+  // },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+// root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
