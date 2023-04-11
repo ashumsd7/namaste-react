c9dd0c8fbc6c321a4884cbba2d1d14c0c5fe766d
aat <aat8001@gmail.com>
2023-04-11 09:56:25 +0530

    understandign rendering and commit phases

--- LetsGetClassyV1/src/components/About.js
+++ LetsGetClassyV1/src/components/About.js
@@ -3,15 +3,30 @@ import { Outlet } from "react-router-dom";
 import Profile from "./ProfileClass";
 import ProfileF from "./Profile";
 
-function About() {
-  return (
-    <div>
-      About
-      <Profile name={'Deva'} />
-      
-      <ProfileF/>
-    </div>
-  );
+// function About() {
+//   return (
+//     <div>
+//       About
+//       <Profile name={"Deva"} />
+//       <ProfileF />
+//     </div>
+//   );
+// }
+
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        About class component
+        <Profile name={"Deva"} />
+        <ProfileF />
+      </div>
+    );
+  }
 }
 
 export default About;
