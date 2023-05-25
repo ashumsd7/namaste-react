00045f2d8bb94625d78fe0b0b6cdf1025d10dbf3
aat <aat8001@gmail.com>
2023-05-25 22:05:53 +0530

    add header

---
+++ machine-coding-round/hello-youtube/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Body from "./components/Body";
+import Head from "./components/Head";
+
+function App() {
+  return (
+    <div className="App">
+      {/* head */}
+      <Head />
+      <Body />
+    </div>
+  );
+}
+
+export default App;
