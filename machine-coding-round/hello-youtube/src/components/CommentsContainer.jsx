ad7c85ce658beaef97078432b92a02d693e0a8e6
aat <aat8001@gmail.com>
2023-06-05 23:21:56 +0530

    WIP: comments

---
+++ machine-coding-round/hello-youtube/src/components/CommentsContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CommentsContainer() {
+  return (
+    <div className='px-4 py-2'>
+        <h1 className='text-2xl font-bold'>Comments:</h1>
+    </div>
+  )
+}
+
+export default CommentsContainer
--- machine-coding-round/hello-youtube/src/components/WatchPage.jsx
+++ machine-coding-round/hello-youtube/src/components/WatchPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSlice";
 import { useSearchParams } from "react-router-dom";
+import CommentsContainer from "./CommentsContainer";
 
 function WatchPage() {
   const dispatch = useDispatch();
@@ -11,18 +12,22 @@ function WatchPage() {
     dispatch(closeMenu());
   }, []);
   return (
-    <div className="px-5 py-2 ">
-      <iframe
-        className="rounded-lg overflow-hidden shadow-lg "
-        width="1000"
-        height="500"
-        src={`https://www.youtube.com/embed/${params.get("v")}`}
-        title="YouTube video player"
-        frameborder="0"
-        autoPlay
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowfullscreen
-      ></iframe>
+    <div className="flex flex-col gap-2">
+      <div className="px-5 py-2 ">
+        <iframe
+          className="rounded-lg overflow-hidden shadow-lg "
+          width="1000"
+          height="500"
+          src={`https://www.youtube.com/embed/${params.get("v")}`}
+          title="YouTube video player"
+          frameborder="0"
+          autoPlay
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+      </div>
+
+      <CommentsContainer/>
     </div>
   );
 }
