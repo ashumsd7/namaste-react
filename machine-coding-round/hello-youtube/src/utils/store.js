a097ee3efea03a47557e862aa5db83219780711f
aat <aat8001@gmail.com>
2023-06-14 21:40:36 +0530

    WIP chats

--- machine-coding-round/hello-youtube/src/utils/store.js
+++ machine-coding-round/hello-youtube/src/utils/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import appSlice from "./appSlice";
 import searchSlice from "./searchSlice";
+import ChatSlice from "./ChatSlice";
 
 const store = configureStore({
   reducer: {
     search:searchSlice,
     app: appSlice,
+    chat:ChatSlice
   },
 });
 
